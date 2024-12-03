b6681806ee6db2dc230ba00099212790a135a1cf
kumackey <kumak1t09e0@gmail.com>
2024-12-03 22:07:37 +0900

    .gitignore

--- main.cc
+++ main.cc
@@ -1,31 +1,52 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-#define MAX 1000000
+#define MAX 1001
+int LCS[MAX][MAX];
+
+int lcs(string X, string Y) {
+    int m = X.size();
+    int n = Y.size();
+    int max = 0;
+
+    X = " " + X;
+    Y = " " + Y;
+    for (int i = 0; i < m; i++) {
+        LCS[i][0] = 0;
+    }
+    for (int j = 0; j < n; j++) {
+        LCS[0][j] = 0;
+    }
+
+    for (int i=1; i<=m; i++) {
+        for (int j=1; j<=n; j++) {
+            if (X[i] == Y[j]) {
+                LCS[i][j] = LCS[i-1][j-1] + 1;
+            } else if (LCS[i-1][j] >= LCS[i][j-1]) {
+                LCS[i][j] = LCS[i-1][j];
+            } else {
+                LCS[i][j] = LCS[i][j-1];
+            }
+
+            if (LCS[i][j] > max) {
+                max = LCS[i][j];
+            }
+        }
+    }
+
+    return max;
+}
 
 int main() {
-    int n, key;
-    cin >> n;
+    int n;
+    scanf("%d", &n);
 
-    int BinaryHeap[MAX];
-
-    for ( int i = 0; i < n; i++) {
-        cin >> BinaryHeap[i+1];
+    for (int i = 0; i < n; i++) {
+        string s1, s2;
+        cin >> s1 >> s2;
+        cout << lcs(s1, s2) << endl;
     }
 
-    for ( int i = 1; i <= n; i++) {
-        cout << "node " << i << ": key = " << BinaryHeap[i] << ", ";
-        if ( i > 1) {
-            cout << "parent key = " << BinaryHeap[i/2] << ", ";
-        }
-        if ( 2*i <= n) {
-            cout << "left key = " << BinaryHeap[2*i] << ", ";
-        }
-        if ( 2*i+1 <= n) {
-            cout << "right key = " << BinaryHeap[2*i+1] << ", ";
-        }
-        cout << endl;
-    }
-    
     return 0;
 }
