ba4c39fe5cfc90e53dade70f70295c204eb7800e
kumackey <kumak1t09e0@gmail.com>
2024-12-07 09:56:19 +0900

    MCM

--- main.cc
+++ main.cc
@@ -1,52 +1,46 @@
 #include <iostream>
 #include <string>
+#include <climits>
 using namespace std;
 
-#define MAX 1001
-int LCS[MAX][MAX];
+#define MAX 101
 
-int lcs(string X, string Y) {
-    int m = X.size();
-    int n = Y.size();
-    int max = 0;
+int M[MAX][MAX];
 
-    X = " " + X;
-    Y = " " + Y;
-    for (int i = 0; i < m; i++) {
-        LCS[i][0] = 0;
-    }
-    for (int j = 0; j < n; j++) {
-        LCS[0][j] = 0;
+int matrixChainMuutiplication(int ps[], int n) {
+    for (int i=1; i<=n; i++) {
+        M[i][i] = 0;
     }
 
-    for (int i=1; i<=m; i++) {
-        for (int j=1; j<=n; j++) {
-            if (X[i] == Y[j]) {
-                LCS[i][j] = LCS[i-1][j-1] + 1;
-            } else if (LCS[i-1][j] >= LCS[i][j-1]) {
-                LCS[i][j] = LCS[i-1][j];
-            } else {
-                LCS[i][j] = LCS[i][j-1];
-            }
-
-            if (LCS[i][j] > max) {
-                max = LCS[i][j];
+    for (int l=2; l<=n; l++) {
+        for (int i=1; i<=n-l+1; i++) {
+            int j =i+l-1;
+            M[i][j] = INT_MAX;
+            for (int k=i; k<=j-1; k++) {
+                M[i][j] = min(M[i][j], M[i][k] + M[k+1][j] + ps[i-1]*ps[k]*ps[j]);
             }
         }
     }
 
-    return max;
+    return M[1][n];
 }
 
 int main() {
     int n;
     scanf("%d", &n);
 
-    for (int i = 0; i < n; i++) {
-        string s1, s2;
-        cin >> s1 >> s2;
-        cout << lcs(s1, s2) << endl;
+    int ps[n+1];
+
+    for (int i=0; i<n; i++) {
+        cin >> ps[i];
+        int tmp = 0;
+        cin >> tmp;
+        if (i == n-1) {
+            ps[i+1] = tmp;
+        }
     }
+
+    printf("%d\n", matrixChainMuutiplication(ps, n));
 
     return 0;
 }
