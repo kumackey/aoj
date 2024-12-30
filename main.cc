75da72f0c9074e5633f96676fa5f3718c3f888ff
kumackey <kumak1t09e0@gmail.com>
2024-12-30 11:17:55 +0900

    射影

--- main.cc
+++ main.cc
@@ -3,83 +3,73 @@
 #include <list>
 #include <queue>
 #include <algorithm>
+#include <cmath>
+#include <iomanip>
 using namespace std;
 
 #define MAX 100000
 
-class Edge {
-public:
-    int s, t, w;
-    Edge() {}
-    Edge(int s, int t, int w) : s(s), t(t), w(w) {}
-};
+#define EPS 1e-10
+#define equals(a, b) (fabs((a) - (b)) < EPS)
 
-class DisjointSet {
+class Point {
     public:
-    vector<int> rank, p;
-    DisjointSet() {}
-    DisjointSet(int size) {
-        rank.resize(size, 0);
-        p.resize(size, 0);
-        for (int i=0;i<size;i++) makeSet(i);
-    }
+        double x, y;
+        Point(double x = 0, double y = 0): x(x), y(y) {}
 
-    void makeSet(int x) {
-        p[x] = x;
-        rank[x] = 0;
-    }
+        Point operator + (Point p) { return Point(x + p.x, y + p.y); }
+        Point operator - (Point p) { return Point(x - p.x, y - p.y); }
+        Point operator * (double a) { return Point(a*x, a*y); }
+        Point operator / (double a) { return Point(x/a, y/a); }
 
-    bool same(int x, int y) {
-        return findSet(x) == findSet(y);
-    }
+        double abs() { return sqrt(norm()); }
+        double norm() { return x*x + y*y; }
 
-    void unite(int x, int y) {
-        link(findSet(x), findSet(y));
-    }
-
-    void link(int x, int y) {
-        if (rank[x]>rank[y]) {
-            p[y] = x;
-        } else {
-            p[x] = y;
-            if (rank[x] == rank[y]) {
-                rank[y]++;
-            }
-        }
-    }
-
-    int findSet(int x) {
-        if (x != p[x]) {
-            p[x] = findSet(p[x]);
+        bool operator < (const Point &p) const {
+            return x != p.x ? x < p.x : y < p.y;
         }
 
-        return p[x];
-    }
+        bool operator == (const Point &p) const {
+            return fabs(x - p.x) < EPS && fabs(y-p.y) < EPS;
+        }
 };
+
+typedef Point Vector;
+
+double dot(Vector a, Vector b) {
+    return a.x*b.x + a.y*b.y;
+}
+
+double cross(Vector a, Vector b) {
+    return a.x*b.y - a.y*b.x;
+}
+
+struct Segment {
+    Point p1, p2;
+};
+
+typedef Segment Line;
+
+Point project(Segment s, Point p) {
+    Vector base = s.p2 - s.p1;
+    double r = dot(p - s.p1, base) / base.norm();
+    return s.p1 + base * r;
+}
 
 int main() {
-    int v, e, s, t, w;
-    cin >> v >> e;
-    vector<Edge> Edges(e);
-    for (int i = 0; i < e; i++) {
-        cin >> s >> t >> w;
-        Edges[i] = Edge(s, t, w);
+    int x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    Segment s = Segment{Point(x1, y1), Point(x2, y2)};
+
+    int q;
+    int x, y;
+
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        cin >> x >> y;
+        Point p = project(s, Point(x, y));
+        cout << fixed << setprecision(10) << p.x << " " << p.y << endl;
     }
 
-    sort(Edges.begin(), Edges.end(), [](Edge a, Edge b) {
-        return a.w < b.w;
-    });
-
-    DisjointSet ds(v);
-    int sum = 0;
-    for (int i = 0; i < e; i++) {
-        Edge e = Edges[i];
-        if (!ds.same(e.s, e.t)) {
-            ds.unite(e.s, e.t);
-            sum += e.w;
-        }
-    }
-    
-    cout << sum << endl;
     return 0;
 }
